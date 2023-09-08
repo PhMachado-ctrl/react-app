7ecf40f5f3bbb9cdc171a688cee231ea4c312cb4
Pedro Henrique <phmachado555@gmail.com>
2023-09-08 19:24:53 -0300

    Cliente

---
+++ src/pages/clientes/index.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+function Cliente (){
+    return(
+        <div>
+            <h1>
+                Cliente em cosntrução...
+            </h1>
+        </div>
+    )
+}
+
+export default Cliente;
