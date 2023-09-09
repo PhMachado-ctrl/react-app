fa235b7e9a6fc5905b3588940a60cd7a89bd2435
Pedro Henrique <phmachado555@gmail.com>
2023-09-09 17:30:23 -0300

    install react-router-dom

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+function Produto (){
+    return(
+        <div>
+            <h1>
+             Produto em cosntrução...
+            </h1>
+        </div>
+    )
+}
+
+export default Produto;
