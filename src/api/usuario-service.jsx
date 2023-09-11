06be4e23a1c676317e0ee92a0feb77d1675ad83d
Pedro Henrique <phmachado555@gmail.com>
2023-09-11 09:04:04 -0300

    autenticar promise

--- src/api/usuario-service.jsx
+++ src/api/usuario-service.jsx
@@ -1,1 +1,8 @@
-import axios from "axios";
+import service from "./service";
+
+// Vai la na API se autenticar devolve os dados
+function autenticar(email, senha){
+    return new Promise((resolve, reject) => {
+
+    })
+}
