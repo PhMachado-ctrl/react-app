5358c14f31555c5702a4e61c030b3a855bea5eb6
Pedro Henrique <phmachado555@gmail.com>
2023-09-11 09:13:47 -0300

    autenticar then/catch

--- src/api/usuario-service.jsx
+++ src/api/usuario-service.jsx
@@ -3,6 +3,8 @@ import service from "./service";
 // Vai la na API se autenticar devolve os dados
 function autenticar(email, senha){
     return new Promise((resolve, reject) => {
-
+        service.post('/login', {email, senha})
+        .then(response => resolve(response))
+        .catch(erro => reject(erro))
     })
 }
